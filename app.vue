b5de8549bafda8a749b8074ed73c4cb4cc760966
didapatria <didapatria3@gmail.com>
2023-07-29 20:56:25 +0700

    change styling

--- app.vue
+++ app.vue
@@ -1,50 +1,48 @@
 <template>
   <div class="flex h-screen bg-gray-100">
-    <div class="m-auto w-11/12 md:w-1/3">
-      <div class="mb-6">
+    <div class="m-auto w-11/12 md:w-1/3 space-y-12">
+      <div>
         <img
           src="./assets/images/logo-fh-removebg-preview-1.png"
           alt="Logo"
           class="mx-auto"
         />
       </div>
-      <h1 class="text-2xl font-bold mb-2 text-center capitalize my-6">
-        Sign In
-      </h1>
-      <p class="text-sm text-gray-500 text-center mb-6">
-        Sistem Informasi Manajemen Keuangan
-      </p>
-      <div class="bg-white shadow-md rounded-lg p-8 my-6">
-        <form>
-          <div class="mb-4">
-            <label
-              for="email"
-              class="block text-sm font-medium text-gray-700 my-2"
-              >Email Address</label
-            >
-            <input
-              type="email"
-              id="email"
-              class="mt-1 px-4 py-2 w-full border rounded-full"
-              placeholder="angga@yourcomp"
-            />
-          </div>
-          <div class="mb-4">
-            <label
-              for="password"
-              class="block text-sm font-medium text-gray-700 my-2"
-              >Password</label
-            >
-            <input
-              type="password"
-              id="password"
-              class="mt-1 px-4 py-2 w-full border rounded-full"
-              placeholder="*************"
-            />
+      <div class="space-y-4 text-center capitalize">
+        <h1 class="text-4xl font-bold mb-2">Sign In</h1>
+        <p class="text-sm text-gray-500">Sistem Informasi Manajemen Keuangan</p>
+      </div>
+      <div class="bg-white shadow-md rounded-lg p-6">
+        <form class="space-y-8">
+          <div class="space-y-4">
+            <div class="space-y-2">
+              <label for="email" class="block text-sm font-medium text-gray-700"
+                >Email Address</label
+              >
+              <input
+                type="email"
+                id="email"
+                class="px-6 py-3 w-full border rounded-full"
+                placeholder="angga@yourcomp"
+              />
+            </div>
+            <div class="space-y-2">
+              <label
+                for="password"
+                class="block text-sm font-medium text-gray-700"
+                >Password</label
+              >
+              <input
+                type="password"
+                id="password"
+                class="px-6 py-3 w-full border rounded-full"
+                placeholder="*************"
+              />
+            </div>
           </div>
           <button
             type="submit"
-            class="bg-blue-500 text-white px-4 py-2 rounded-full w-full my-2"
+            class="bg-blue-500 text-white px-4 py-2 rounded-full w-full"
           >
             Sign in
           </button>
